1cd87aa295e29343a1cd73a414eee583914d4276
Zain Imran <zainimran@outlook.com>
2022-02-16 20:48:38 +0500

    add rules + convert & push script + update action

---
+++ script.py
@@ -0,0 +1,91 @@
+import yaml
+import requests
+import json
+import sys
+
+# parse a sigma rule in a yaml file and get its contents
+with open(sys.argv[1], 'r') as stream:
+    try:
+        data_loaded = yaml.safe_load(stream)
+        name = data_loaded['title']
+        description = data_loaded['description']
+        pattern_severity = data_loaded['level']
+        product = data_loaded['logsource']['product']
+        category = data_loaded['logsource']['category']
+        detection = data_loaded['detection']
+    except yaml.YAMLError as exc:
+        print(exc)
+
+ruletype_id = "0"
+if product == 'windows':
+    if category == 'dns_query':
+        ruletype_id = "11"
+    elif category == 'process_creation':
+        ruletype_id = '5'
+
+field_values = []
+
+if 'filter' in detection:
+    image_filter = ''
+    if 'Image|contains' in detection['filter']:
+        image_filter = ".*{detection}.*".format(
+            detection=detection['filter']['Image|contains'])
+        field_values.append({
+            "name": "ImageFilename",
+            "label": "Image Filename",
+            "type": "excludable",
+            "values": [{
+                "label": "exclude",
+                "value": image_filter
+            }
+            ]
+        })
+
+if 'dns_request' in detection:
+    dns_values = []
+    if 'QueryName' in detection['dns_request']:
+        dns_list = detection['dns_request']['QueryName']
+        for dns in dns_list:
+            dns_values.append({
+                "label": "include",
+                "value": dns
+            })
+    elif 'QueryName|contains' in detection['dns_request']:
+        dns_list = detection['dns_request']['QueryName|contains']
+        for dns in dns_list:
+            dns = ".*{detection}.*".format(detection=detection['dns_request']['QueryName|contains'])
+            dns_values.append({
+                "label": "include",
+                "value": dns
+            })
+    if len(dns_values) == 0:
+      dns_values = ".*"
+    field_values.append({
+        "name": "DomainName",
+        "label": "Domain Name",
+        "type": "excludable",
+        "values": dns_values
+    })
+
+payload = json.dumps({
+    "name": name,
+    "comment": description,
+    "description": description,
+    "pattern_severity": pattern_severity,
+    "rulegroup_id": "cd2f65a62b7241478ca9a2f9aded6c63",
+    "ruletype_id": ruletype_id,
+    "disposition_id": 10,
+    "field_values": field_values
+})
+
+headers = {
+    'X-CS-USERNAME': 'zain.imran@ebryx.com',
+    'Authorization': 'Bearer {token}'.format(token=sys.argv[2]),
+    'Content-Type': 'application/json'
+}
+
+url = "https://api.us-2.crowdstrike.com/ioarules/entities/rules/v1"
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
