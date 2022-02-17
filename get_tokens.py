d64c950d77b630828d3389bb3bd8171ec36892f5
Zain Imran <zainimran@outlook.com>
2022-02-17 18:39:58 +0500

    test 10

--- get_tokens.py
+++ get_tokens.py
@@ -4,25 +4,28 @@ import json
 import sys
 import os
 
-print(os.environ['SECRETS'])
+with open('config.yml', 'r') as stream:
+    try:
+        data_loaded = yaml.safe_load(stream)
+        cs_base_url = data_loaded['cs_base_url']
+        cs_client_id = data_loaded['cs_client_id']
+        cs_client_secret = data_loaded['cs_client_secret']
+        cs_auth_token_file = data_loaded['cs_auth_token_file']
+    except yaml.YAMLError as exc:
+        print(exc)
 
+client_id = os.environ[cs_client_id]
+client_secret = os.environ[cs_client_secret]
 
-# with open('config.yml', 'r') as stream:
-#     try:
-#         data_loaded = yaml.safe_load(stream)
-#         cs_base_url = data_loaded['cs_base_url']
-#     except yaml.YAMLError as exc:
-#         print(exc)
+url = "{base_url}/oauth2/token".format(base_url=cs_base_url)
 
+payload='client_id={client_id}&client_secret={client_secret}'.format(client_id=client_id, client_secret=client_secret)
+headers = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/x-www-form-urlencoded'
+}
 
-# url = "{base_url}/oauth2/token".format(base_url=cs_base_url)
+response = requests.request("POST", url, headers=headers, data=payload)
 
-# payload='client_id=c0f5df9a9efb4c7b8c96f9737652b780&client_secret=Xr5fkwR3yLCSKJ7PB24Etos0QMb98lDWz61aAdVZXr5fkwR3yLCSKJ7PB24Etos0QMb98lDWz61aAdVZ'
-# headers = {
-#   'Accept': 'application/json',
-#   'Content-Type': 'application/x-www-form-urlencoded'
-# }
-
-# response = requests.request("POST", url, headers=headers, data=payload)
-
-# print(response.text)
+with open(cs_auth_token_file, 'w') as outfile:
+    json.dump(response.json(), outfile)
